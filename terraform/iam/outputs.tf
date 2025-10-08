ec70f78473771009d94ae0f92107eb3f66a42a9f
pengchao <pengchao.ma2@outlook.com>
2025-10-08 21:02:15 +0800

    modified the arn

--- terraform/iam/outputs.tf
+++ terraform/iam/outputs.tf
@@ -8,8 +8,3 @@ output "eks_node_group_role_arn" {
   description = "ARN of the EKS node group IAM role"
   value       = aws_iam_role.eks_node_group.arn
 }
-
-output "alb_controller_policy_arn" {
-  description = "ARN of the ALB Controller IAM policy"
-  value       = aws_iam_policy.alb_controller.arn
-}
