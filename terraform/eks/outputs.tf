062753a4e565dca3a8352a7bb3bd189f2469704f
pengchao <pengchao.ma2@outlook.com>
2025-10-08 16:11:30 +0800

    modified

--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -52,8 +52,3 @@ output "cluster_oidc_issuer_url" {
   description = "OIDC issuer URL for the EKS cluster"
   value       = aws_eks_cluster.main.identity[0].oidc[0].issuer
 }
-
-output "cluster_certificate_authority_data" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = aws_eks_cluster.main.certificate_authority[0].data
-}
