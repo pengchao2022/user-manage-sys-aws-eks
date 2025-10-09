572842db0cd7eac922ffaaf40698e74a1ea558e4
pengchao <pengchao.ma2@outlook.com>
2025-10-09 08:38:54 +0800

    modified again

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -22,7 +22,7 @@ resource "aws_eks_node_group" "main" {
   instance_types = var.instance_types
 
   scaling_config {
-    desired_size = 2
+    desired_size = 4
     max_size     = 5
     min_size     = 1
   }
